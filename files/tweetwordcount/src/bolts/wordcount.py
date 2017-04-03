0bd26d187a848b54dedee71278786c199d8ef10b
Timothy Alt <timothymalt@gmail.com>
2017-04-02 22:26:55 -0700

    Update wordcount.py

--- files/tweetwordcount/src/bolts/wordcount.py
+++ files/tweetwordcount/src/bolts/wordcount.py
@@ -5,8 +5,7 @@ from streamparse.bolt import Bolt
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-#Connecting to tcount
-conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+
 
 class WordCounter(Bolt):
 
@@ -15,7 +14,7 @@ class WordCounter(Bolt):
 
     def process(self, tup):
         word = tup.values[0]
-
+        
         # Write codes to increment the word count in Postgres
         # Use psycopg to interact with Postgres
         # Database name: Tcount 
@@ -25,13 +24,16 @@ class WordCounter(Bolt):
         # Increment the local count
         self.counts[word] += 1
         self.emit([word, self.counts[word]])
+
+        
+        #Connecting to tcount
+        conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
         
         # Updating tcount
         cur = conn.cursor()
         cur.execute("INSERT INTO tweetwordcount (word, count)
                     SELECT word, self.counts[word]
-                    WHERE NOT EXISTS (SELECT word FROM tweetwordcount WHERE word=%s", (word))
-                    
+                    WHERE NOT EXISTS (SELECT word FROM tweetwordcount WHERE word=%s", (word))            
         cur.execute("UPDATE tweetwordcount SET count=%s WHERE word=%s", (word, self.counts[word]))                 
         conn.commit()
         
