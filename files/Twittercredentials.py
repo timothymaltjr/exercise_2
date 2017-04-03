f9887e00c66a0f41e8841ea500ef5852502c3f8b
Timothy Alt <timothymalt@gmail.com>
2017-04-02 18:34:03 -0700

    Update Twittercredentials.py

--- files/Twittercredentials.py
+++ files/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
+consumer_key = "pcMKYjdGsOWjcdOUqJ31LpaO6";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "YourConsumerSecret";
+consumer_secret = "8Ahq11BxzDt1ZN3Hx6wFd81Ua0eQKOAzqhTtXDSQzxLD4CXkxy";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "YourAcessToken";
+access_token = "872491214-apayJ073FxanNvtLatRA3VzQKWAMhv1eOnThJD1A";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "YourTokenSecret";
+access_token_secret = "	s5hGT1mcB7KKyemueeqFJPcguurSm7aaM8kZtRQMauOUn";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
